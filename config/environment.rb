0cbe5690f4b53c0b1595b822dc56629ce9b939e5
DATDANG <foxan3011@gmail.com>
2017-03-02 11:00:54 +0000

    Build Homepage

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => 'plain',
+   :user_name => "tughe3011@gmail.com",
+   :password => "123456"
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,12 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.default_url_options = { :host => 'photo-app-foxan3011.c9users.io' }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default :charset => "utf-8"
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -38,4 +42,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  
+
 end
